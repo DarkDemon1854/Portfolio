568616d44ed597a78a6273d5e4ef9e2eeffcffea
DarkDemon1854 <207335599+DarkDemon1854@users.noreply.github.com>
2025-06-17 11:26:18 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
     title: "Stream",
     image: project2,
     description: 
-      "Developed a dynamic web-based movie streaming platform that fetches films from a Telegram channel, leveraging The Movie Database (TMDb) API for enriched metadata such as posters, thumbnails, and detailed movie information. The platform supports seamless online streaming, playback via MX Player and VLC, and direct file downloads. Designed with an intuitive interface, smooth navigation, optimized video performance, and real-time content updates.\n\nLogin to explore:\n• Username: `Raj`\n• Password: `Raj`",
+      "Developed a dynamic web-based movie streaming platform that fetches films from a Telegram channel, leveraging The Movie Database (TMDb) API for enriched metadata including posters, thumbnails, and detailed movie info. Integrated Google Firebase for real-time database syncing, user authentication, and scalable backend services. Implemented monetized short links using popular URL shortener APIs (e.g., ShrinkMe, Linkvertise) with both backend and frontend support to generate revenue. The platform enables seamless online streaming, playback via MX Player and VLC, and direct file downloads. Designed with an intuitive UI, smooth navigation, optimized video performance, and real-time content updates.",
     technologies: [
       "Python",
       "HTML",
@@ -81,8 +81,11 @@ export const PROJECTS = [
       "LocalStorage",
       "MongoDB",
     ],
-    demoLink: "https://stream98-6f17d9813837.herokuapp.com/",
-    githubLink: "https://github.com/DarkDemon1854/Stream"
+    demoLink: "https://songoku.vercel.app/",
+    githubLink: {
+      frontend: "https://github.com/DarkDemon1854/Stream-Frontend",
+      backend: "https://github.com/DarkDemon1854/Stream-Backend",
+    }
   },
 ];
 
